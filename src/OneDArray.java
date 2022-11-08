fa85a56d34227a843db737f0c28f95574eb847b5
Digamber singh <testingdemo8617@gmail.com>
2022-11-08 21:16:40 +0530

    Digamber : first code push

---
+++ src/OneDArray.java
@@ -0,0 +1,9 @@
+public class OneDArray {
+    public static void main(String[] args) {
+        int arr[]={1,3,4,5,767,56};
+        for (int i=0;i< arr.length;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
